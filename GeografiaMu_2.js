790f0e88d150b07b411a9fb3e3e3bc6bec19500a
alerubira <100098255+alerubira@users.noreply.github.com>
2023-09-18 19:55:29 -0300

    falta

--- GeografiaMu_2.js
+++ GeografiaMu_2.js
@@ -1,4 +1,4 @@
-const paises = [];
+let paises = [];
 function P(nombre, capital, bandera) {
   this.nombre = nombre;
   this.capital = capital;
@@ -16,29 +16,35 @@ let inicio=document.getElementById("inicio");
 let alias=document.getElementById("alias");
 let bandera;
 let aliasObligatorio=document.getElementById("aliasObligatorio");
+let cantidad;
+let paisesJugar;
+const aleatorio=(al)=>{
+  return Math.floor(Math.random() * al) ;
+}
 const llenarDiv=(a,b)=>{
     a.innerHTML=`<img src="${b}" width="${200}" height="${100}"  />`;
   }
   const llenarDivC=(a,b)=>{
     a.textContent=b;
   }
-  const llenarDivs=(j,c)=>{
+  const llenarDivs=(j,c1,c2,c3,c4)=>{
           
     if(j=="adivCapital"){
-      div1.innerHTML=paises[paisesJugar[c]].capiltal[0];
-      div2.innerHTML=paises[paisesJugar[c]].capiltal[0];
-      div3.innerHTML=paises[paisesJugar[c]].capiltal[0];
-      div4.innerHTML=paises[paisesJugar[c]].capiltal[0];
+      div1.innerHTML=paises[paisesJugar[c1]].capital[0];
+      div2.innerHTML=paises[paisesJugar[c2]].capital[0];
+      div3.innerHTML=paises[paisesJugar[c3]].capital[0];
+      div4.innerHTML=paises[paisesJugar[c4]].capital[0];
 
     } else{
-      div1.innerHTML=`<img src="${paises[paisesJugar[c]].bandera}" width="${200}" height="${100}"  />` ;
-      div2.innerHTML= `<img src="${paises[paisesJugar[c]].bandera}" width="${200}" height="${100}"  />`;
-      div3.innerHTML= `<img src="${paises[paisesJugar[c]].bandera}" width="${200}" height="${100}"  />`;
-      div4.innerHTML= `<img src="${paises[paisesJugar[c]].bandera}" width="${200}" height="${100}"  />`;
+      div1.innerHTML=`<img src="${paises[paisesJugar[c1]].bandera}" width="${200}" height="${100}"  />` ;
+      div2.innerHTML= `<img src="${paises[paisesJugar[c2]].bandera}" width="${200}" height="${100}"  />`;
+      div3.innerHTML= `<img src="${paises[paisesJugar[c3]].bandera}" width="${200}" height="${100}"  />`;
+      div4.innerHTML= `<img src="${paises[paisesJugar[c4]].bandera}" width="${200}" height="${100}"  />`;
     }
 }
-const llenarArray=(al,tam,arr)=>{//llena un arreglo de 40 para jugar
+ llenarArray=(al,tam)=>{//llena un arreglo de 40 para jugar
     var bandera=true;
+    var arr =[];
     do{
       var a=aleatorio(al);
         if(!arr.includes(a)){
@@ -48,6 +54,40 @@ const llenarArray=(al,tam,arr)=>{//llena un arreglo de 40 para jugar
           bandera=false;
         }
     }while(bandera);
+    return arr;
+}
+const vaciar=()=>{
+  div1.innerHTML="";
+  div2.innerHTML="";
+  div3.innerHTML="";
+  div4.innerHTML="";
+  adivinaBandera.innerHTML="";
+  adivinaCapital.innerHTML="";
+  aliasObligatorio.innerHTML="";
+}
+const jugar=(c,cI)=>{
+  var d =aleatorio(4);
+  var c1,c2,c3,c4;
+  switch (d) {
+    case 0:
+      c1=c;c2=cI;c3=cI+1;c4=cI+2;
+      break;
+    case 1:
+      c1=cI;c2=c;c3=cI+1;c4=+2;
+      break;
+    case 2:
+      c1=cI;c2=cI+1;c3=c;c4=cI+2;
+      break;
+    case 3:
+      c1=cI;c2=cI+1;c3=cI+2;c4=c;
+      break;
+    
+  if(aleatorio(2)==1){
+      eligeBandera(c1,c2,c3,c4);
+  }else{
+    eligeCapital(c1,c2,c3,c4);
+  }
+}
 }
  
 fetch("https://restcountries.com/v3.1/all")
@@ -60,7 +100,7 @@ fetch("https://restcountries.com/v3.1/all")
       p1= new P(pais.name.official,pais.capital,pais.flags.svg);
       
       paises.push(p1);
-      
+       cantidad=paises.length-1;
         
     });
     //console.log(paises[0]);
@@ -76,67 +116,51 @@ fetch("https://restcountries.com/v3.1/all")
     //console.log(ruta);
     });
     
-     
-      const aleatorio=(al)=>{
-        return Math.floor(Math.random() * al) ;
-      }
-      
-      const vaciar=()=>{
-        div1.innerHTML="";
-        div2.innerHTML="";
-        div3.innerHTML="";
-        div4.innerHTML="";
-        adivinaBandera.innerHTML="";
-        adivinaCapital.innerHTML="";
-      }
     
-    const cantidad=paises.length-1;
+
     inicio.addEventListener("click", function() {
            aliasObligatorio.innerHTML="";
            if(alias.value==""){
             
             aliasObligatorio.innerHTML="el alias debe se obligatorio";
            }else{
+            paisesJugar=llenarArray(cantidad,43);//llena un arreglo con 40 numeros para buscar los paises para jugar 
             empezar();
            }
           
           });
- let paisesJugar=[];
  
- //llenarArray(cantidad,40,paisesJugar);//llena un arreglo con 40 numeros para buscar los paises para jugar
+ 
+ //
  function empezar(){
   var cont=0;//para seleccionar paises para acertar
   var cantI=10;//para seleccionar paises para rellenar
+  vaciar();
   do{
-     jugar(cont);
+     jugar(cont,cantI);
      
      cont++;
+     cantI=cantI+3;
+     //vaciar();
   }while(cont>10);
 
  }
-const jugar=(c)=>{
-  
-  vaciar();
-  if(aleatorio(2)==1){
-      eligeBandera(c);
-  }else{
-    eligeCapital(c);
-  }
 
-}
-const eligeCapital=(c)=>{
+
+
+const eligeCapital=(c1,c2,c3,c4)=>{
   adivinaCapital.textContent="Adivina Capital";
   //var posPais=aleatorio(cantidad);
   nombrePais.textContent=paises[paisesJugar[c]].nombre;
-  //llenarDivC(div1,paises[paisesJugar[]].capital[0]);
-  //llenarDivC(div2,paises[paisesJugar[]].capital[0]);
-  //llenarDivC(div3,paises[paisesJugar[]].capital[0]);
-  //llenarDivC(div4,paises[paisesJugar[]].capital[0]);
-  llenarDivs("adivinaCapital",c)
+ // llenarDivC(div1,paises[paisesJugar[c]].capital[0]);
+ // llenarDivC(div2,paises[paisesJugar[cI]].capital[0]);
+ // llenarDivC(div3,paises[paisesJugar[cI+1]].capital[0]);
+ // llenarDivC(div4,paises[paisesJugar[cI+3]].capital[0]);
+  llenarDivs("adivCapital",c,cI)
 }
-const eligeBandera=(c)=>{
+const eligeBandera=(c1,c2,c3,c4)=>{
   adivinaBandera.textContent="Adivina Bandera";
   nombrePais.textContent=paises[paisesJugar[c]].nombre;
-  llenarDivs("adivi",c)
+  llenarDivs("adivi",c,cI)
 }
 
